72d7a711a761d63f7b4bf8aae817996114914fce
mironal <miro.gunp@gmail.com>
2023-02-04 16:38:13 +0900

    Clients

--- Sources/MastodonKit/Requests/Clients.swift
+++ Sources/MastodonKit/Requests/Clients.swift
@@ -8,28 +8,28 @@
 
 import Foundation
 
-/// `Clients` requests.
-public enum Clients {
-    /// Registers an application.
-    ///
-    /// - Parameters:
-    ///   - appName: Name of your application.
-    ///   - redirectURI: Where the user should be redirected after authorization (for no redirect, omit this parameter).
-    ///   - scopes: Application's access scopes.
-    ///   - website: URL to the homepage of your app.
-    /// - Returns: Request for `ClientApplication`.
-    public static func register(clientName: String,
-                                redirectURI: String = "urn:ietf:wg:oauth:2.0:oob",
-                                scopes: [AccessScope],
-                                website: String? = nil) -> Request<ClientApplication> {
-        let parameters = [
-            Parameter(name: "client_name", value: clientName),
-            Parameter(name: "redirect_uris", value: redirectURI),
-            Parameter(name: "website", value: website),
-            Parameter(name: "scopes", value: scopes.map(toString).joined(separator: " "))
-        ]
-
-        let method = HTTPMethod.post(.parameters(parameters))
-        return Request<ClientApplication>(path: "/api/v1/apps", method: method)
+extension MastodonRequests {
+    /// `Clients` requests.
+    public enum Clients {
+        /// Registers an application.
+        open class Register: RequestBase<ClientApplication> {
+            /// - Parameters:
+            ///   - appName: Name of your application.
+            ///   - redirectURI: Where the user should be redirected after authorization (for no redirect, omit this parameter).
+            ///   - scopes: Application's access scopes.
+            ///   - website: URL to the homepage of your app.
+            public init(clientName: String,
+                        redirectURI: String = "urn:ietf:wg:oauth:2.0:oob",
+                        scopes: [AccessScope],
+                        website: String? = nil) {
+                let parameters = [
+                    Parameter(name: "client_name", value: clientName),
+                    Parameter(name: "redirect_uris", value: redirectURI),
+                    Parameter(name: "website", value: website),
+                    Parameter(name: "scopes", value: scopes.map(toString).joined(separator: " "))
+                ]
+                super.init(path: "/api/v1/apps", method: .post(.parameters(parameters)))
+            }
+        }
     }
 }
